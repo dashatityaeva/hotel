1f39153e2c34cc6ba43ebd867ae062d21f030894
Daria <tityaevadash@gmail.com>
2021-01-25 14:44:53 +0300

    Создала бургер меню

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const menu = document.querySelectorAll('.nav__item-list');
+
+//active link of menu
+const clearLink = function () {
+    menu.forEach(item => item.classList.remove('nav__item-list_active-top'));
+}
+menu.forEach(item => {
+    item.addEventListener('click', function() {
+        clearLink();
+        item.classList.add('nav__item-list_active-top')
+    })
+})
+
+
+const toggleBtn = document.querySelector('.nav-toggle');
+const menuActive = document.querySelector('.header__wrap');
+const socialsActive = document.querySelector('.header__socials');
+
+toggleBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    this.classList.toggle('nav-toggle_active');
+    menuActive.classList.toggle('menu-active');
+    socialsActive.classList.toggle('header__socials-active');
+})
